45e485af821d0ecd6b9e8d451d55559c754dc713
Lea@Mac <leaseep@gmail.com>
2023-02-16 19:30:40 +0100

    still do not kow if this is over complicated but Im drunk

--- utils.R
+++ utils.R
@@ -1,20 +1,25 @@
 # Draw Lunch Time
 
-# 1. draw a minute between 1:30 (sample)
-# 2. put in the new lunch time 11:30 + 1.
-# 3. check if it is tuesday (every two weeks)
-
-getLunchTime <- function(){
-  if(weekdays(Sys.Date())=="Tuesday" & SkipThisWeek){
-    result <- "It is Group Meeting! Lunch time heavily depends on length of such, 
-               which is by no means predictable..."
-    SkipThisWeek <<- F
-  }else{
-    drawnMinutes <- sample(1:30,1)
-    startTime <- as.POSIXct("12:30:00", format = "%H:%M:%S")
-    new_time <- as.POSIXct(startTime + (60 * drawnMinutes))
-    LunchTime <- format(new_time, format = "%H:%M")
-    result <- paste0("Today's Lunch Time is: ",LunchTime)
+getLunchTime <- function(dayRequest){
+  if(lastDayRequest != dayRequest){
+    if(dayRequest == "Tuesday" & !SkipThisWeek){
+      TodaysLunchTime <<- "It is Group Meeting! Lunch time heavily depends on length of such, 
+                 which is by no means predictable..."
+      SkipThisWeek <<- T
+    }else if(dayRequest %in% c("Saturday","Sunday")){
+      TodaysLunchTime <<- "It is the Weekend. You have to care for your lunch yourself"
+    }else{
+      drawnMinutes <- sample(1:30,1)
+      startTime <- as.POSIXct("12:30:00", format = "%H:%M:%S")
+      new_time <- as.POSIXct(startTime + (60 * drawnMinutes))
+      LunchTime <- format(new_time, format = "%H:%M")
+      TodaysLunchTime <<- paste0("Today's Lunch Time is: ",LunchTime)
+      if(dayRequest == "Tuesday"){
+        SkipThisWeek <<- F
+      }
+    }
+    
+    lastDayRequest <<- dayRequest
   }
-  return(result)
+  return(TodaysLunchTime)
 }
