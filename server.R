483a5ec0f9dc23d260ac3c9f96d057cd7d7db510
Lea@Mac <leaseep@gmail.com>
2023-02-16 19:29:19 +0100

    do this bunch to get lunch

--- server.R
+++ server.R
@@ -1,16 +1,24 @@
 # server ----
 source("utils.R")
+TodaysLunchTime <<- "No Lunch"
+lastDayRequest <<- "none"
+SkipThisWeek <<- TRUE
 
 server <- function(input, output, session) {
   
   # current time
   output$output <- renderText({
     invalidateLater(1000, session)
-    paste("The current time is", format(Sys.time(), "%H:%M:%S"))
+    paste("current time: ", format(Sys.time(), "%H:%M:%S"))
   })
-  
+   
+  # Update Lunch Time only once a day
+  ToDay <- weekdays(Sys.Date())
+
   #
   output$TodaysLunchTime <- renderText({
-    getLunchTime()
+    getLunchTime(ToDay)
   })
 }
+
+
