342fb7365c07a94bf19eee3062a6850a888a4ca0
Lea@Mac <leaseep@gmail.com>
2023-02-17 08:26:45 +0100

    Now we can also access the beautiful stats

--- server.R
+++ server.R
@@ -19,6 +19,40 @@ server <- function(input, output, session) {
   output$TodaysLunchTime <- renderText({
     getLunchTime(ToDay)
   })
+  
+  output$ActualLunch_UI <- renderUI({
+    if(actualLunch_recorder){
+      actionButton("ActualLunch", "Click to record actual Lunch Time",icon = icon("check"))
+    }else{
+      NULL
+    }
+  })
+  
+  observeEvent(input$ActualLunch,{
+    file <- readLines("www/LunchTimeOverview.csv")
+    length <- length(readLines("www/LunchTimeOverview.csv"))
+    last_line <- file[length] 
+    last_line <- gsub("NA$",format(Sys.time(), format = "%H:%M"),last_line)
+    file[length] <- last_line
+    write(
+      file,
+      file = "www/LunchTimeOverview.csv",
+      sep = ","
+    )
+    actualLunch_recorder <<- F
+    output$ActualLunch_UI <- renderUI({NULL})
+    
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste0("LunchTime_Stats_",format(Sys.Date(),format = "%y_%m_%d"),".csv")
+    },
+    content = function(file) {
+      write.csv(read.csv("www/LunchTimeOverview.csv"), file, row.names = FALSE)
+    }
+  )
+  
 }
 
 
